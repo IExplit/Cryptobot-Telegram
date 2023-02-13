64c8e98b60f71e0e28d5f52107a7e1a7cdf083df
IExplit <124705005+IExplit@users.noreply.github.com>
2023-02-13 21:18:15 +0500

    Update starting.py

--- handlers/starting.py
+++ handlers/starting.py
@@ -5,4 +5,4 @@ from database.editdata import  add_user, get_user
 async def start(message):
     await add_user(message)
     await get_user(message)
-    await bot.send_message(message.chat.id, 'priv')
+    await bot.send_message(message.chat.id, 'Команда для вызова меню: /menu \nДобавить валюту: /add (код валюты) \nУдалить валюту: /remove (код валюты) \n\nДоступные валюты можно посмотреть на сайте: https://www.coingecko.com/ru')
